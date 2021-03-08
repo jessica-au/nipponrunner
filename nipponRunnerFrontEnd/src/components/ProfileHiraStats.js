ffce89fe3c956fff74051d5c5f17a12954a977ee
Leonidas <ldbrooks297@gmail.com>
2021-03-08 12:36:47 -0800

    ProfileHiraStats.js:
    -began basic div structuring for grid progress view
    
    -need to finish rest of left chart and begin right chart
    
    -will need styling to display chart as flex
    
    -hirachart left as flex colum
    
    -each set of sounds as flex row
    
    -each character with viewer appropriate sizing
    
    -will need functionalitiy to change color based on db.user.incorrect

---
+++ nipponRunnerFrontEnd/src/components/ProfileHiraStats.js
@@ -0,0 +1,263 @@
+import React from 'react';
+
+const ProfileHiraStats = (props) => {
+
+    return (
+        <div className="hiraganaStats">
+            <div className="hiraChart">
+                <div className="hiraChartLeft">
+
+                    <div className="vowelSounds">
+                        <div className="vowelSoundsA">
+                            あ
+                        </div>
+                        <div className="vowelSoundsI">
+                            い
+                        </div>
+                        <div className="vowelSoundsU">
+                            う
+                        </div>
+                        <div className="vowelSoundsE">
+                            え
+                        </div>
+                        <div className="vowelSoundsO">
+                            お
+                        </div>
+                    </div>
+
+                    <div className="kSounds">
+                        <div className="kSoundsA">
+                            か
+                        </div>
+                        <div className="kSoundsI">
+                            き
+                        </div>
+                        <div className="kSoundsU">
+                            く
+                        </div>
+                        <div className="kSoundsE">
+                            け
+                        </div>
+                        <div className="kSoundsO">
+                            こ
+                        </div>
+                    </div>
+
+                    <div className="sSounds">
+                        <div className="sSoundsA">
+                            さ
+                        </div>
+                        <div className="sSoundsI">
+                            し
+                        </div>
+                        <div className="sSoundsU">
+                            す
+                        </div>
+                        <div className="sSoundsE">
+                            せ
+                        </div>
+                        <div className="sSoundsO">
+                            そ
+                        </div>
+                    </div>
+
+                    <div className="tSounds">
+                        <div className="tSoundsA">
+                            た
+                        </div>
+                        <div className="tSoundsI">
+                            ち
+                        </div>
+                        <div className="tSoundsU">
+                            つ
+                        </div>
+                        <div className="tSoundsE">
+                            て
+                        </div>
+                        <div className="tSoundsO">
+                            と
+                        </div>
+                    </div>
+
+                    <div className="nSounds">
+                        <div className="nSoundsA">
+                            な
+                        </div>
+                        <div className="nSoundsI">
+                            に
+                        </div>
+                        <div className="nSoundsU">
+                            ぬ
+                        </div>
+                        <div className="nSoundsE">
+                            ね
+                        </div>
+                        <div className="nSoundsO">
+                            の
+                        </div>
+                    </div>
+
+                    <div className="hSounds">
+                        <div className="hSoundsA">
+                            は
+                        </div>
+                        <div className="hSoundsI">
+                            ひ
+                        </div>
+                        <div className="hSoundsU">
+                            ふ
+                        </div>
+                        <div className="hSoundsE">
+                            へ
+                        </div>
+                        <div className="hSoundsO">
+                            ほ
+                        </div>
+                    </div>
+
+                    <div className="mSounds">
+                        <div className="mSoundsA">
+                            ま
+                        </div>
+                        <div className="mSoundsI">
+                            み
+                        </div>
+                        <div className="mSoundsU">
+                            む
+                        </div>
+                        <div className="mSoundsE">
+                            め
+                        </div>
+                        <div className="mSoundsO">
+                            も
+                        </div>
+                    </div>
+
+                    <div className="ySounds">
+                        <div className="ySoundsA">
+                            や
+                        </div>
+
+                        <div className="charBox"></div>
+
+                        <div className="ySoundsU">
+                            ゆ
+                        </div>
+
+                        <div className="charBox"></div>
+
+                        <div className="ySoundsO">
+                            よ
+                        </div>
+                    </div>
+
+                    <div className="rSounds">
+                        <div className="rSoundsA">
+                            ら
+                        </div>
+                        <div className="rSoundsI">
+                            り
+                        </div>
+                        <div className="rSoundsU">
+                            る
+                        </div>
+                        <div className="rSoundsE">
+                            れ
+                        </div>
+                        <div className="rSoundsO">
+                            ろ
+                        </div>
+                    </div>
+
+                    <div className="wSounds">
+                        <div className="wSoundsA">
+                            わ
+                        </div>
+
+                        <div className="charBox"></div>
+                        <div className="charBox"></div>
+                        <div className="charBox"></div>
+
+                        <div className="wSoundsO">
+                            を
+                        </div>
+                    </div>
+
+                    <div className="nCharacter">
+                        ん
+                    </div>
+
+                    <div className="gSounds">
+                        がぎぐげご
+                    </div>
+
+                    <div className="zSounds">
+                        ざじずぜぞ
+                    </div>
+
+                    <div className="dSounds">
+                        だぢづでど
+                    </div>
+
+                    <div className="bSounds">
+                        ばびぶべぼ
+                    </div>
+
+                    <div className="pSounds">
+                        ぱぴぷぺぽ
+                    </div>
+                </div>
+                <div className="hiraChartRight">
+                    <div className="morphSounds">
+
+                        <div className="morphSoundsKy">
+
+                        </div>
+
+                        <div className="morphSoundsGy">
+
+                        </div>
+
+                        <div className="morphSoundsSh">
+
+                        </div>
+
+                        <div className="morphSoundsJ">
+
+                        </div>
+
+                        <div className="morphSoundsCh">
+
+                        </div>
+
+                        <div className="morphSoundsNy">
+
+                        </div>
+                        
+                        <div className="morphSoundsHy">
+
+                        </div>
+
+                        <div className="morphSoundsBy">
+
+                        </div>
+
+                        <div className="morphSoundsMy">
+
+                        </div>
+
+                        <div className="morphSoundsPy">
+
+                        </div>
+
+                        <div className="morphSoundsRy">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ProfileHiraStats;
