3d407cd7af519ea06d83ac7716a26725fc97ae5a
jessica-au <AU.JESSICA.M@GMAIL.COM>
2021-03-08 19:46:33 -0800

    create link for hiragana from gakkou component

--- nipponRunnerFrontEnd/src/components/Gakkou/Hiragana.js
+++ nipponRunnerFrontEnd/src/components/Gakkou/Hiragana.js
@@ -1,21 +1,24 @@
 //imports
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Link } from 'react-router-dom';
 
 
 //functions
 //function for clicking hira divs
-function Square(props) {
+// function Square(props) {
+//     return (
+//         <button className="hiraBox" onClick={props.onClick}>click
+//         </button>
+//     );
+// }
+
+
+
+const Hiragana = () => {
+
     return (
-        <button className="hiraBox" onClick={props.onClick}>click
-        </button>
-    );
-}
-
-
-
-const Hiragana = (props) => {
-    return (
+        
         <div className="hiraContainer">
             <div className="hiraRow">
                 <div className="hiragana">
@@ -27,17 +30,30 @@ const Hiragana = (props) => {
                 <div className="hiragana">
                     う
             </div>
-            </div>
-            <div className="hiraRow">
-
                 <div className="hiragana">
                     え
             </div>
                 <div className="hiragana">
                     お
              </div>
-
             </div>
+            {/* <div className="hiraRow">
+                <div className="hiragana">
+                    a
+            </div>
+                <div className="hiragana">
+                    i
+            </div>
+                <div className="hiragana">
+                    u
+            </div>
+                <div className="hiragana">
+                    e
+            </div>
+                <div className="hiragana">
+                    o
+             </div>
+             </div> */}
         </div>
     );
 }
