d72463a00884d668317ee90556ea1a7e4a432f93
jessica-au <AU.JESSICA.M@GMAIL.COM>
2021-03-09 14:39:28 -0800

    req not working for gakkou.js

--- nipponRunnerFrontEnd/src/components/Gakkou/Gakkou.js
+++ nipponRunnerFrontEnd/src/components/Gakkou/Gakkou.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom'; 
 import Hiragana from './Hiragana';
+import axios from 'axios';
+
+const { REACT_APP_SERVER_URL } = process.env;
+
 
 const Gakkou = (props) => {
+  // const hira= axios.get(`${REACT_APP_SERVER_URL}/${req.body.bango}`);
   
     return (
         <div className="gakkou">
             <h1>Okaeri! Welcome!</h1>
+            
             <div className="information">
             <Link to="/gakkou/hiragana">
                 <button type="button" className="hiraButton">
--- nipponRunnerFrontEnd/src/components/Gakkou/Hiragana.js
+++ nipponRunnerFrontEnd/src/components/Gakkou/Hiragana.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Link } from 'react-router-dom';
+import axios from 'axios'
 
 
 //functions
