2574a8353882b502ed7861211fa294636f083617
jessica-au <AU.JESSICA.M@GMAIL.COM>
2021-03-12 15:45:53 -0800

    move backend into main repo, delete frontend dir

--- nipponRunnerFrontEnd/src/tests/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from '../App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const h1Element = screen.getByText(/にっぽんランナー/i);
-  expect(h1Element).toBeInTheDocument();
-});
--- nipponRunnerFrontEnd/src/tests/Welcome.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Welcome from '../components/Welcome';
-
-test('render the Welcome header', () => {
-    render(<Welcome />);
-    const h1Element = screen.getByText(/Welcome/i);
-    expect(h1Element).toBeInTheDocument();
-});
